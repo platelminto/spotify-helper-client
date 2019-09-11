c2678a2ae134db02b96836209f3c43b4de599610
Giorgio Momigliano <gmomigliano@protonmail.com>
2019-09-11 18:24:25 +0100

    Keyboard listener no longer needs to be in it's own thread

--- src/main/taskbar_icon.py
+++ src/main/taskbar_icon.py
@@ -1,7 +1,5 @@
 # Is the main app, and needs to be the main thread for pystray to work correctly.
 import os
-import threading
-import time
 import subprocess
 import platform
 import logging
@@ -9,40 +7,29 @@ import logging
 from PIL import Image
 from pystray import Icon, Menu, MenuItem
 from src.main.spotify_helper import SpotifyHelper, bindings_file
-
+from ..notifications.notif_handler import send_notif
 
 logging.basicConfig(filename='../spotify-helper.log', level=logging.INFO,
                     format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
 
-# Begins the keyboard listener.
-class SpotifyThread(threading.Thread):
-    def __init__(self):
-        super(SpotifyThread, self).__init__()
-        self.daemon = True
-
-    def run(self):
-        SpotifyHelper().run()
-
-
-spotify_thread = SpotifyThread()
-spotify_thread.start()
-
-
 # Opens the bindings file in the default text editor
 def open_bindings_file():
+    send_notif('Changing bindings.', 'Please restart the Spotify Helper app after \
+                                                                saving your changes')
     current_os = platform.system()
-    if current_os == 'Darwin':       # macOS
+    if current_os == 'Darwin':  # macOS
         subprocess.call(('open', bindings_file))
-    elif current_os == 'Windows':    # Windows
+    elif current_os == 'Windows':  # Windows
         os.startfile(bindings_file)
-    else:                                   # Linux
+    else:  # Linux
         subprocess.call(('xdg-open', bindings_file))
 
 
 if __name__ == "__main__":
-    icon_image = Image.open('../resources/spo.png')
+    SpotifyHelper().run()
 
+    icon_image = Image.open('../resources/spo.png')
     icon = Icon('spotify-helper', icon_image, menu=Menu(
         MenuItem(
             text='Edit bindings',
@@ -53,17 +40,5 @@ if __name__ == "__main__":
             action=lambda: icon.stop()
         ),
     ))
-
-    # If the spotify_helper thread crashed, the program should exit completely.
-    def is_helper_alive():
-        while True:
-            if not spotify_thread.is_alive():
-                break
-            time.sleep(3)
-        icon.stop()
-
-    check_helper_alive_thread = threading.Thread(target=is_helper_alive)
-    check_helper_alive_thread.daemon = True
-    check_helper_alive_thread.start()
 
     icon.run()
