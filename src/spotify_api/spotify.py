e8169c4bf2b52b787fb67960ea909c7615a61d17
Giorgio Momigliano <gmomigliano@protonmail.com>
2019-09-15 19:19:17 +0100

    Added support for paginated responses and all requests are now handled similarly

--- src/spotify_api/spotify.py
+++ src/spotify_api/spotify.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 import platform
 import shelve
 import configparser
@@ -33,7 +34,7 @@ class Spotify:
         redirect_uri = 'https://platelminto.eu.pythonanywhere.com/users/registering'
 
         scope_list = ['user-library-read', 'user-library-modify', 'playlist-modify-public',
-                      'user-modify-playback-state', 'user-read-playback-state']
+                      'user-modify-playback-state', 'user-read-playback-state', 'playlist-modify-private']
 
         self.web_api = WebApi(scope_list=scope_list, client_id=client_id,
                               redirect_uri=redirect_uri)
@@ -49,17 +50,17 @@ class Spotify:
         self.repeat_states = ['track', 'context', 'off']
 
     def next(self):
-        self.try_local_method_then_web('next', 'next', 'post')
+        self.try_local_method_then_web('next', 'me/player/next', 'post')
 
     def previous(self):
-        self.try_local_method_then_web('previous', 'previous', 'post')
+        self.try_local_method_then_web('previous', 'me/player/previous', 'post')
 
     # Starting a song over means setting its current playing-time to 0.
     def restart(self):
-        self.try_local_method_then_web('restart', 'seek', 'put', params={'position_ms': 0})
+        self.try_local_method_then_web('restart', 'me/player/seek', 'put', params={'position_ms': 0})
 
     def pause(self):
-        self.try_local_method_then_web('pause', 'pause', 'put')
+        self.try_local_method_then_web('pause', 'me/player/pause', 'put')
 
     def toggle_play(self):
         try:
@@ -78,7 +79,7 @@ class Spotify:
         # endpoint to "transfer" playback to the already active device, which allows us to give it
         # a 'play' parameter to resume playback. This is done in call_play_method() to ensure
         # an API call is made only if a local API isn't available.
-        self.try_local_method_then_web('play', '', 'put')
+        self.try_local_method_then_web('play', 'me/player', 'put')
 
     # This does not toggle save, so if a song is already saved it doesn't remove it.
     def save(self):
@@ -107,10 +108,10 @@ class Spotify:
         def change_shuffle_with_web_api(response):
             toggled_shuffle = not response.json().get('shuffle_state')
 
-            self.web_api.put('me/player/shuffle', params={'state': toggled_shuffle})
+            self.call_web_method('me/player/shuffle', 'put', params={'state': toggled_shuffle})
             send_notif('Shuffle toggled', 'Shuffle now ' + ('enabled' if toggled_shuffle else 'disabled'))
 
-        self.try_local_method_then_web('toggle_shuffle', '', 'get', change_shuffle_with_web_api)
+        self.try_local_method_then_web('toggle_shuffle', 'me/player', 'get', change_shuffle_with_web_api)
 
     def toggle_repeat(self):
         # There are 3 repeat states (off, track, context), so we cannot simply toggle
@@ -119,17 +120,17 @@ class Spotify:
             repeat_state = response.json().get('repeat_state')
 
             next_state = self.repeat_states[self.repeat_states.index(repeat_state) - 1]
-            self.web_api.put('me/player/repeat', params={'state': next_state})
+            self.call_web_method('me/player/repeat', 'put', params={'state': next_state})
             send_notif('Repeat changed', 'Repeating is now set to: ' + next_state)
 
-        self.try_local_method_then_web('toggle_repeat', '', 'get', change_state_with_web_api, 'get')
+        self.try_local_method_then_web('toggle_repeat', 'me/player', 'get', change_state_with_web_api, 'get')
 
     def play_on_current_device(self):
-        self.call_player_method('', 'put', payload={'device_ids': [self.get_current_device_id()]})
+        self.call_web_method('me/player', 'put', payload={'device_ids': [self.get_current_device_id()]})
 
     def toggle_save_monthly_playlist(self):
         song_id = self.get_current_song_id()
-        is_in_playlist = self.is_in_playlist(song_id)
+        is_in_playlist = self.is_in_playlist(song_id, 0)
 
         song = self.get_current_song_info()[0]
 
@@ -149,21 +150,25 @@ class Spotify:
         send_notif_with_web_image(song, ', '.join(artists) + ' - ' + album, self.currently_playing_art_url())
 
     def add_song_to_monthly_playlist(self, song_id):
-        return self.web_api.post(
-            'users/' + self.get_user_id() + '/playlists/' + self.get_monthly_playlist_id() + '/tracks',
-            params={'uris': 'spotify:track:' + song_id})
+        return self.call_web_method(
+            'users/{}/playlists/{}/tracks'.format(self.get_user_id(), self.get_monthly_playlist_id()),
+            'post',
+            params={'uris': 'spotify:track:{}'.format(song_id)}
+        )
 
-    # The API is inconsitent so adding and deleting are different.
+    # The API is inconsistent so adding and deleting are different.
     def remove_song_from_monthly_playlist(self, song_id):
-        return self.web_api.delete(
-            'users/' + self.get_user_id() + '/playlists/' + self.get_monthly_playlist_id() + '/tracks',
-            payload={'tracks': [{'uri': 'spotify:track:' + song_id}]})
+        return self.call_web_method(
+            'users/{}/playlists/{}/tracks'.format(self.get_user_id(), self.get_monthly_playlist_id()),
+            'delete',
+            payload={'tracks': [{'uri': 'spotify:track:{}'.format(song_id)}]}
+        )
 
     def get_current_song_info(self):
         def get_track_from_web_api(response):
             return response.json().get('item')
 
-        track = self.try_local_method_then_web('get_current_track', '', 'get', get_track_from_web_api)
+        track = self.try_local_method_then_web('get_current_track', 'me/player', 'get', get_track_from_web_api)
 
         song = track.get('name')
         artists = [x.get('name') for x in track.get('artists')]
@@ -180,10 +185,10 @@ class Spotify:
             # playlist id.
             if 'month' not in shelf or shelf['month'] != month:
                 shelf['month'] = month
-                shelf['monthly_playlist_id'] = self.__fetch_playlist_id(month, year)
+                shelf['monthly_playlist_id'] = self.__fetch_playlist_id(month, year, 0)
             if 'year' not in shelf or shelf['year'] != year:
                 shelf['year'] = year
-                shelf['monthly_playlist_id'] = self.__fetch_playlist_id(month, year)
+                shelf['monthly_playlist_id'] = self.__fetch_playlist_id(month, year, 0)
             return shelf['monthly_playlist_id']
 
     def get_user_id(self):
@@ -193,30 +198,52 @@ class Spotify:
             return shelf['user_id']
 
     def __fetch_user_id(self):
-        return self.web_api.get('me').json().get('id')
+        return self.call_web_method('me', 'get').json().get('id')
 
-    def __fetch_playlist_id(self, month, year):
-        response = self.web_api.get('me/playlists').json()
+    def __fetch_playlist_id(self, month, year, offset):
+        response = self.call_web_method('me/playlists', 'get').json()
+
+        playlist_id = None
 
         for playlist in response.get('items'):
-            if str(playlist.get('name')).lower() == (month + ' ' + year).lower():
-                return playlist.get('id')
+            if str(playlist.get('name')).lower() == '{} {}'.format(month, year).lower():
+                playlist_id = playlist.get('id')
 
-        return self.web_api.post('users/' + self.get_user_id() + '/playlists',
-                                 payload={'name': (month.capitalize() + ' ' + year)}).json().get('id')
+        if playlist_id is not None:
+            return playlist_id
+        elif playlist_id is None and Spotify.is_paging(response):
+            return self.__fetch_playlist_id(month, year, offset + response.get('limit'))
+        else:
+            return self.call_web_method(
+                'users/{}/playlists'.format(self.get_user_id()),
+                'post',
+                payload={'name': '{} {}'.format(month.capitalize(), year)}
+            ).json().get('id')
 
-    def is_in_playlist(self, song_id):
-        playlist_tracks = self.web_api.get('users/' + self.get_user_id() +
-                                           '/playlists/' + self.get_monthly_playlist_id() +
-                                           '/tracks').json().get('items')
+    def is_in_playlist(self, song_id, offset):
+        playlist_tracks = self.call_web_method(
+            'playlists/{}/tracks'.format(self.get_monthly_playlist_id()),
+            'get',
+            params={'offset': offset}
+        ).json()
 
-        return len([x for x in playlist_tracks if x.get('track').get('id') == song_id]) > 0
+        exists = len([x for x in playlist_tracks.get('items') if x.get('track').get('id') == song_id]) > 0
+
+        if not exists and Spotify.is_paging(playlist_tracks):
+            return self.is_in_playlist(song_id, offset + playlist_tracks.get('limit'))
+        else:
+            return exists
+
+    @staticmethod
+    def is_paging(json):
+        return json.get('next') is not None
 
     def add_songs_to_library(self, *song_ids):
-        return self.web_api.put('me/tracks', payload={'ids': song_ids})
+        return self.call_web_method('me/tracks', 'put', payload={'ids': song_ids})
+        # return self.web_api.put('me/tracks', payload={'ids': song_ids})
 
     def get_available_devices(self):
-        return self.web_api.get('me/player/devices').json().get('devices')
+        return self.call_web_method('me/player/devices', 'get').json().get('devices')
 
     # The active device is the one currently playing music, not necessarily the one currently
     # being used by the user (for example, music could be playing through a phone, but the user
@@ -233,15 +260,16 @@ class Spotify:
         def get_id_from_web_api(response):
             return response.json().get('item').get('id')
 
-        return self.try_local_method_then_web('get_track_id', '', 'get', get_id_from_web_api, 'get')
+        return self.try_local_method_then_web('get_track_id', 'me/player', 'get', get_id_from_web_api, 'get')
 
     def is_saved(self, song_id):
-        return self.web_api.get('me/tracks/contains?ids=' + song_id).json()[0]
+        # Returns a list of boolean values matching each id we give it; with only one, we get the first and only value
+        return self.call_web_method('me/tracks/contains', 'get', params={'ids': [song_id]}).json()[0]
 
     def currently_playing_art_url(self, track=None, quality=2):
         if track is None:
             try:
-                track = self.try_local_method_then_web('', '', 'get').json().get('item')
+                track = self.try_local_method_then_web('', 'me/player', 'get').json().get('item')
 
             except ConnectionError:
                 return None
@@ -253,18 +281,18 @@ class Spotify:
         return images[-quality if len(images) >= 2 else 0].get('url')
 
     def remove_songs_from_library(self, *song_ids):
-        return self.web_api.delete('me/tracks', payload={'ids': song_ids})
+        return self.call_web_method('me/tracks', 'delete', payload={'ids': song_ids})
 
     def is_playing(self):
-        return self.try_local_method_then_web('is_playing', '', 'get').json().get('is_playing')
+        return self.try_local_method_then_web('is_playing', 'me/player', 'get').json().get('is_playing')
 
     # Utility method for possible future use.
     def get_shuffle_and_repeat_state(self):
-        response = self.web_api.get('me/player').json()
+        response = self.call_web_method('me/player', 'get').json()
         return response.get('shuffle_state'), response.get('repeat_state')
 
     def get_current_device_id(self):
-        return next(x.get('id') for x in self.call_player_method('devices', 'get').json().get('devices') if
+        return next(x.get('id') for x in self.call_web_method('me/player/devices', 'get').json().get('devices') if
                     x.get('name') == get_device_name())
 
     # For every method, we first try a local API, and then move onto the Web API as a fallback.
@@ -275,32 +303,48 @@ class Spotify:
 
         except AttributeError:
             return do_with_web_result(
-                self.call_player_method(web_method_name, rest_function_name, params=params, payload=payload))
+                self.call_web_method(web_method_name, rest_function_name, params=params, payload=payload))
 
-    def call_player_method(self, method, rest_function_name, params=None, payload=None):
+    def call_web_method(self, method, rest_function_name, params=None, payload=None):
         # 'get' functions don't have payloads.
         if rest_function_name == 'get':
-            response = getattr(self.web_api, rest_function_name)('me/player/' + method, params=params)
-        # get_active_devices() is here to avoid unnecessarily calls if not using the Web API.
-        elif method == '' and rest_function_name == 'put':
-            response = getattr(self.web_api, rest_function_name)('me/player/' + method, params=params,
+            response = getattr(self.web_api, rest_function_name)(method, params=params)
+        # get_active_devices() is here to avoid unnecessarily calls if not using the Web API when calling play(),
+        # which has no payload.
+        elif method == 'me/player' and rest_function_name == 'put':
+            response = getattr(self.web_api, rest_function_name)(method, params=params,
                                                                  payload={
                                                                      'device_ids': [self.get_active_device().get('id')],
-                                                                     'play': True})
+                                                                     'play': True} if payload is None else payload)
         else:
-            response = getattr(self.web_api, rest_function_name)('me/player/' + method, params=params, payload=payload)
+            response = getattr(self.web_api, rest_function_name)(method, params=params, payload=payload)
 
         status_code = response.status_code
 
-        if status_code is 403:
-            send_notif('Error', 'Must be premium')
-            raise AlreadyNotifiedException()
-
         # 'get' with no further method returns information about the user's playback.
         # 204s are usually successes, but in this case it means no active devices exist.
-        if status_code is 204 and method == '' and rest_function_name == 'get':
+        if status_code == 204 and method == 'me/player' and rest_function_name == 'get':
             send_notif('Error', 'No device found')
-            raise AlreadyNotifiedException()
+            raise AlreadyNotifiedException
+        elif 200 <= status_code <= 299:  # These responses are fine
+            return response
+
+        info = response.json()
+
+        # Player errors also return a reason, which we use to notify with the appropriate message from config.ini,
+        # though it appears nearly all reasons will always be UNKNOWN, an issue with the Spotify API.
+        if 'error' in info and 'reason' in info.get('error'):
+            reason = info.get('error').get('reason')
+            config = configparser.ConfigParser()
+            config.read('../config.ini')
+            response = config['player_error_strings'][reason]
+            send_notif('Player Error', response)
+            raise AlreadyNotifiedException
+
+        if status_code >= 300:
+            logging.warning('Request {} failed with code {}'.format(response.text, status_code))
+            logging.warning('Fail message: {}'.format(response.json().get('error').get('message')))
+            raise Exception
 
         return response
 
